62728a978e419bf50cb12fd9ff58b96d9034b479
Tulasi-Reddy29 <tulasisasanoolareddy@gmail.com>
2025-01-30 09:36:27 +0530

    Add files via upload

---
+++ conditionalstatements.java
@@ -0,0 +1,12 @@
+public class conditionalstatements {
+    public static void main(String[] args) {
+        int a = -2345;
+        if (a > 0) {
+            System.out.println("the number is positive");
+        } else if (a < 0) {
+            System.out.println("the number is negtaive");
+        } else
+            System.out.println("its 0");
+    }
+
+}
